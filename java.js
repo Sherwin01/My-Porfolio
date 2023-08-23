1246551b0cda2ea6ce6289b5bb6b9080b4e6a519
lim <sherwinlim8899@gmail.com>
2023-08-23 22:00:32 +0800

    adding-bootstrap-to-my-project

--- java.js
+++ java.js
@@ -1,43 +1,17 @@
+// google sheet
+const scriptURL = 'https://script.google.com/macros/s/AKfycbznNBo_kIXCMRGgwB37Zab6lR9YeD9QW4cfFeoFASwSVdWenv_q7zr8N9pULVWl57_q/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg")
 
-
-var tablinks = document.getElementsByClassName("tab-links");
-var tabcontents = document.getElementsByClassName("tab-contents");
-
-function opentab(tabname){
-    for(tablink of tablinks){
-        tablink.classList.remove("active-link");
-    }
-    for(tabcontent of tabcontents){
-        tabcontent.classList.remove("active-tab");
-    }
-    event.currentTarget.classList.add("active-link");
-    document.getElementById(tabname).classList.add("active-tab");
-}
-
-var sidemenu = document.getElementById("sidemenu");
-
-function openmenu() {
-    sidemenu.style.right = "0";
-}
-
-function closemenu() {
-    sidemenu.style.right = "-200px";
-}
-
-
-  const scriptURL = 'https://script.google.com/macros/s/AKfycbznNBo_kIXCMRGgwB37Zab6lR9YeD9QW4cfFeoFASwSVdWenv_q7zr8N9pULVWl57_q/exec'
-  const form = document.forms['submit-to-google-sheet']
-  const msg = document.getElementById("msg")
-
-  form.addEventListener('submit', e => {
-    e.preventDefault()
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-      .then(response => {
-            msg.innerHTML = "Message sent successfully"
-            setTimeout(function(){
-                msg.innerHTML = ""
-            },5000)
-            form.reset()
-      })
-      .catch(error => console.error('Error!', error.message))
-  })
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+          msg.innerHTML = "Message sent successfully"
+          setTimeout(function(){
+              msg.innerHTML = ""
+          },5000)
+          form.reset()
+    })
+    .catch(error => console.error('Error!', error.message))
+})
